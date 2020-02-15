afcbcf5481105be7c675713e2934840e71c46a2a
Vivica Ma <vivicacongma@gmail.com>
2020-02-15 20:29:17 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -28,10 +28,10 @@ end
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
   my_hash_creator = {
-    key: "value"
+    dog: "woof"
   }
 end
-my_hash_creator[cat, "Meow"]
+
 
 def read_from_hash(hash, key)
   # return the correct value using the hash and key parameters
